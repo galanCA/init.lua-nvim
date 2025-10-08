739320e6e821f4768a85c62b6c49365be11bd627
galanca <cesar.galan@colorado.edu>
2025-10-08 15:16:46 -0600

    Add keymap for method.

--- lua/galanca/plugins/formatting.lua
+++ lua/galanca/plugins/formatting.lua
@@ -5,13 +5,26 @@ return {
         local conform = require("conform")
 
         conform.setup({
+            log_level = vim.log.levels.DEBUG,
             formatters_by_ft = {
-                lua = { "stylua" }
+                lua = { "stylua" },
+                go = { "goimports" }
             },
             format_on_save = {
                 timeout_ms = 500,
                 lsp_fallbadck = true,
             },
+            formatters = {
+                goimports = {
+                    command = '"C:\\Users\\Cesar Galan\\AppData\\Local\\nvim-data\\mason\\bin\\goimports.cmd"'
+                }
+            }
         })
+        vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+            conform.format({
+                timeout_ms = 500,
+                lsp_fallbadck = true,
+            })
+        end, { desc = "Format file or range (in visual mode)" })
     end
 }
